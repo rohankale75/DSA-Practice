2c8149d394168314c7a48f8384ef34a8294ba341
rokale <rokale@synoptek.com>
2025-09-28 13:55:20 +0530

    Added 5 new patterns from 12 to 16

--- DSAStriver/1. Patterns/Pattern.cs
+++ DSAStriver/1. Patterns/Pattern.cs
@@ -4,6 +4,26 @@ namespace DSAStriver._1._Patterns
 {
     public class Pattern
     {
+        public void AccessPattern()
+        {
+            print1(5);
+            print2(5);
+            print3(5);
+            print4(5);
+            print5(5);
+            print6(5);
+            print7(5);
+            print8(5);
+            print9(5);
+            print10(5);
+            print11(5);
+            print12(5);
+            print13(5);
+            print14(5);
+            print15(6);
+            print16(5);
+        }
+
         public void Spacing(string patternName)
         {
             Console.WriteLine();
@@ -182,6 +202,90 @@ namespace DSAStriver._1._Patterns
                     Console.Write(Count);
                     Count = 1 - Count;
                 }
+                Console.WriteLine();
+            }
+        }
+
+        public void print12(int n)
+        {
+            Spacing("12: Numbers Space Number Triangle pattern");
+            for (int i = 1; i <= n; i++)
+            {  // Outer loop for Rows
+
+                // numbers
+                for (int j = 1; j <= i; j++)
+                {  // for left side numbers
+                    Console.Write(j);
+                }
+
+                // Spaces
+
+                for (int k = 0; k < 2 * (n - i); k++)   // Formula 2 * (n - 1) OR 2 * (n - i)
+                {
+                    Console.Write("-");
+                }
+
+                // numbers
+                for (int l = i; l >= 1; l--)
+                {
+                    Console.Write(l);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void print13(int n)
+        {
+            Spacing("13: Incremental Numbers Triangle");
+            int count = 1;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write(count + " ");
+                    count++;
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void print14(int n)
+        {
+            Spacing("14: Characters triangle pattern");
+            for (int i = 1; i <= n; i++)
+            {
+                for (char ch = 'A'; ch < 'A' + i; ch++)
+                {
+                    Console.Write(ch);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void print15(int n)
+        {
+            Spacing("15: Reverse characters triangle pattern");
+            for (int i = 1; i <= n; i++)
+            {
+                for (char ch = 'A'; ch < 'A' + (n - i); ch++)
+                {
+                    Console.Write(ch);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void print16(int n)
+        {
+            Spacing("16: characters Triangle Pattern");
+            char ch = 'A';
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write(ch);
+                }
+                ch++;
                 Console.WriteLine();
             }
         }
--- DSAStriver/Program.cs
+++ DSAStriver/Program.cs
@@ -2,14 +2,4 @@
 using DSAStriver._1._Patterns;
 
 Pattern pattern = new Pattern();
-pattern.print1(5);
-pattern.print2(5);
-pattern.print3(5);
-pattern.print4(5);
-pattern.print5(5);
-pattern.print6(5);
-pattern.print7(5);
-pattern.print8(5);
-pattern.print9(5);
-pattern.print10(5);
-pattern.print11(5);
+pattern.AccessPattern();
