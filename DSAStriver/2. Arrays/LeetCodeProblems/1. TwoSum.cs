747bac9ca70e90ddcbe1c5b02877f602e3c722b1
rokale <rokale@synoptek.com>
2025-10-23 23:28:04 +0530

    Added Problmes set to few questions

--- DSAStriver/2. Arrays/LeetCodeProblems/1. TwoSum.cs
+++ DSAStriver/2. Arrays/LeetCodeProblems/1. TwoSum.cs
@@ -1,7 +1,47 @@
-﻿namespace DSAStriver._2._Arrays.LeetCodeProblems
+﻿using System.Diagnostics;
+using System.Runtime.Intrinsics.X86;
+using System.Security.Cryptography;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace DSAStriver._2._Arrays.LeetCodeProblems
 {
     public class TwoSum
     {
+        #region 1. Two Sum Array 
+
+        //Hint
+        //Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+        //You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+        //You can return the answer in any order.
+
+        //Example 1:
+
+        //Input: nums = [2, 7, 11, 15], target = 9
+        //Output: [0, 1]
+        //Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+        //Example 2:
+
+        //Input: nums = [3, 2, 4], target = 6
+        //Output: [1, 2]
+        //Example 3:
+
+        //Input: nums = [3, 3], target = 6
+        //Output: [0, 1]
+
+
+        //Constraints:
+
+        //2 <= nums.length <= 104
+        //-109 <= nums[i] <= 109
+        //-109 <= target <= 109
+        //Only one valid answer exists.
+
+
+        //Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+        #endregion
+
         // Demo Array int[] arr = { 10, 20, 4, 7, 5 };
 
         #region 1. O(n^2) Time Complexity
